b28b8e7ceb6f93c09d84a809145742740d691bdc
jrezuke <j.rezuke@verizon.net>
2017-06-21 16:29:33 -0400

    added sites-bootstrap-font-awsome-jquery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from "app/shared/shared.module";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    SharedModule.forRoot()
+    SharedModule.forRoot() //this is available in lazy load mods
   ],
   providers: [],
   bootstrap: [AppComponent]
