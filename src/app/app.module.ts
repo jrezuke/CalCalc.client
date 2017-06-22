385457923443fffbfc4bc7b8ef71e155ab13d367
jrezuke <j.rezuke@verizon.net>
2017-06-22 15:59:25 -0400

    added home - sites is working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from "app/shared/shared.module";
 import { HomeComponent } from './home/home.component';
+import { SitesService } from "app/sites/sites.service";
 
 @NgModule({
   declarations: [
@@ -16,9 +18,12 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    HttpModule,
     SharedModule.forRoot() //this is available in lazy load mods
   ],
-  providers: [],
+  providers: [
+    SitesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
