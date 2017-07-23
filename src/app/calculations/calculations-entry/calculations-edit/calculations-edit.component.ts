0ba9bcc5bc43315bbc2f331a4f2b1dbd4f1f81b3
jrezuke <j.rezuke@verizon.net>
2017-07-23 17:52:08 -0400

    got calculationsBarItems working

--- src/app/calculations/calculations-entry/calculations-edit/calculations-edit.component.ts
+++ src/app/calculations/calculations-entry/calculations-edit/calculations-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SidebarItem } from "app/shared/sidebar/sidebar-item/sidebar-item";
+import { LayoutService } from "app/shared/layout.service";
 
 @Component({
   selector: 'calculations-edit',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculations-edit.component.css']
 })
 export class CalculationsEditComponent implements OnInit {
-
-  constructor() { }
+  calcbarItems: SidebarItem[];
+  constructor(private _layoutService: LayoutService) { }
 
   ngOnInit() {
+     this._layoutService.getSidebarItems('entry')
+        .subscribe((res: SidebarItem[]) => {
+          this.calcbarItems = res;
+          console.log('calcbarItems', this.calcbarItems);
+        },
+        (e) => console.log('error:', e));  
+        
   }
 
 }
