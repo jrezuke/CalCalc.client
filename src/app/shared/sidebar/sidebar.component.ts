1768ea37fdfcd74e928ea899790a665d79f94bd2
jrezuke <j.rezuke@verizon.net>
2017-07-24 16:11:35 -0400

    working on sidebar styling

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -4,7 +4,8 @@ import { SidebarItem } from "app/shared/sidebar/sidebar-item/sidebar-item";
 @Component({
   selector: 'sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
+  
 })
 export class SidebarComponent implements OnInit {
 @Input() sidebarItems: SidebarItem[];
